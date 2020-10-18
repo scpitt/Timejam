21fe8e406cb7f37a7a8448fd4582750a073963be
scpitt <56580891+scpitt@users.noreply.github.com>
2020-10-19 06:11:27 +1100

    Update 433mhz_remote_timejam.ino

--- 433mhz_remote_timejam/433mhz_remote_timejam.ino
+++ 433mhz_remote_timejam/433mhz_remote_timejam.ino
@@ -3,6 +3,7 @@
 // Arduino Mega, connect pin 52 to either of the two middle pins of a 433MHz Receiver Module,
 // connect pin 53 to 5v on Digispark with DigisparkJammerTimer code,
 // connect negative from Digispark and Reciever Module to GND on Mega.
+// ( see   https://youtu.be/r8ci0oNYmOA   for demo )
 
 void setup() {
   Serial.begin(9600);
